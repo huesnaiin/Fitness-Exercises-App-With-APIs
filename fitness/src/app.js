0188293d01a4e466fa60ffc44d17176dc18884cc
Husnain Aslam <114501183+HusnaiinASLAM@users.noreply.github.com>
2022-11-06 05:09:32 +0500

    Hy!

---
+++ fitness/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import {Route,Routes} from 'react-router-dom'
+import ExcerciseDetail from './pages/ExcerciseDetail'
+import Home from './pages/Home'
+
+const app = () => {
+  return (
+    <div>Hi this is husnaiin</div>
+  )
+}
+
+export default app
